ff1cc40a729387f1a2262c984fc4f3aacf908a1b
Ning Ma <ningma905@gmail.com>
2022-06-30 19:52:29 +0100

    adding new dog not completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,110 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+const state = { dogs: [...data] };
+
+const setState = (update) => {
+  Object.keys(update).forEach((prop) => {
+    state[prop] = update[prop];
+  });
+
+  render();
+};
+
 const dogsUL = document.querySelector(".dogs-list");
+const addDogBtn = document.querySelector(".dogs-list__button--add");
 const mainDogSection = document.querySelector(".main__dog-section");
 
+const createAddCard = () => {
+  mainDogSection.innerHTML = "";
+
+  const heading = document.createElement("h2");
+  mainDogSection.appendChild(heading);
+  heading.innerText = "Add a new Dog";
+
+  const form = document.createElement("form");
+  mainDogSection.appendChild(form);
+  form.classList.add("form");
+
+  const nameLabel = document.createElement("label");
+  form.appendChild(nameLabel);
+  nameLabel.setAttribute("for", "name");
+  nameLabel.innerText = "Dog's name";
+
+  const nameInput = document.createElement("input");
+  form.appendChild(nameInput);
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("id", "name");
+  nameInput.setAttribute("name", "name");
+
+  const imgLabel = document.createElement("label");
+  form.appendChild(imgLabel);
+  imgLabel.setAttribute("for", "image");
+  imgLabel.innerText = "Dog's picture";
+
+  const imgInput = document.createElement("input");
+  form.appendChild(imgInput);
+  imgInput.setAttribute("type", "url");
+  imgInput.setAttribute("id", "image");
+  imgInput.setAttribute("name", "image");
+
+  const bioLabel = document.createElement("label");
+  form.appendChild(bioLabel);
+  bioLabel.setAttribute("for", "bio");
+  bioLabel.innerText = "Dog's bio";
+
+  const bioInput = document.createElement("textarea");
+  form.appendChild(bioInput);
+  bioInput.setAttribute("rows", 5);
+  bioInput.setAttribute("id", "bio");
+  bioInput.setAttribute("name", "bio");
+
+  const submit = document.createElement("input");
+  form.appendChild(submit);
+  submit.classList.add("form__button");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("id", "submit");
+  submit.setAttribute("name", "submit");
+  submit.setAttribute("value", "Let's add a dog!");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const id = state.dogs.length + 1;
+    const name = event.target[0].value;
+    const img = event.target[1].value;
+    const bio = event.target[2].value;
+    const newDog = {
+      id: id,
+      name: name,
+      bio: bio,
+      isGoodDog: true,
+      image: img,
+    };
+
+    const match = state.dogs.find(
+      (dog) => dog.name === name && dog.bio === bio && dog.image === img
+    );
+
+    if (match) {
+      alert("This dog is already on the App.");
+      return;
+    }
+
+    const myDogs = [newDog, ...state.dogs];
+
+    console.log(myDogs);
+    console.log(dogsUL);
+
+    setState({ dogs: myDogs });
+  });
+};
+
+addDogBtn.addEventListener("click", () => {
+  createAddCard();
+});
+
+// Header
 const createDogLi = (dog) => {
   const dogLi = document.createElement("li");
   dogLi.classList.add("dogs-list__button");
@@ -14,6 +115,25 @@ const createDogLi = (dog) => {
   });
 
   return dogLi;
+};
+
+const render = () => {
+  //   state.dogs.forEach((dog) => {
+  //     dogsUL.appendChild(createDogLi(dog));
+  //   });
+
+  for (let i = 0; i < state.dogs.length; i++) {
+    dogsUL.appendChild(createDogLi(state.dogs[i]));
+  }
+};
+
+// Main
+const toggleGood = (dog) => {
+  if (dog.isGoodDog) {
+    dog.isGoodDog = false;
+  } else {
+    dog.isGoodDog = true;
+  }
 };
 
 const createDogCard = (dog) => {
@@ -27,7 +147,8 @@ const createDogCard = (dog) => {
   mainDogSection.appendChild(img);
   img.setAttribute("src", dog.image);
   img.setAttribute("alt", "");
-  img.style.height = "300px";
+  img.style.height = "250px";
+  img.style.width = "380px";
 
   const dogInfoWrap = document.createElement("div");
   mainDogSection.appendChild(dogInfoWrap);
@@ -39,8 +160,7 @@ const createDogCard = (dog) => {
 
   const infoText = document.createElement("p");
   dogInfoWrap.appendChild(infoText);
-  infoText.innerText =
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, minima voluptates libero cumque rerum consequatur optio aliquid sint eum maxime illo laborum omnis quo ab rem cupiditate nulla perspiciatis ipsum!";
+  infoText.innerText = dog.bio;
 
   const goodDogWrap = document.createElement("div");
   dogInfoWrap.appendChild(goodDogWrap);
@@ -49,11 +169,17 @@ const createDogCard = (dog) => {
   const goodDogText = document.createElement("p");
   goodDogWrap.appendChild(goodDogText);
   const goodDogTextEm = document.createElement("em");
-  goodDogTextEm.innerText = "Is naughty?";
   goodDogText.appendChild(goodDogTextEm);
+  goodDogTextEm.innerText = "Is naughty?";
 
   const goodDogBtn = document.createElement("button");
   goodDogWrap.appendChild(goodDogBtn);
+  goodDogBtn.addEventListener("click", () => {
+    toggleGood(dog);
+    goodDogText.innerHTML = "";
+    goodDogText.appendChild(goodDogTextEm);
+    checkGoodDog(dog);
+  });
 
   const checkGoodDog = (dog) => {
     if (dog.isGoodDog) {
@@ -68,10 +194,4 @@ const createDogCard = (dog) => {
   checkGoodDog(dog);
 };
 
-const renderDogUL = () => {
-  data.forEach((dog) => {
-    dogsUL.appendChild(createDogLi(dog));
-  });
-};
-
-renderDogUL();
+render();
